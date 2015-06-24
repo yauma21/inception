4e97ff13f600adbb5bdde10cb8ae953a6be37155
Jaime Jorge <jaime.jorge@scmspain.com>
2015-06-24 18:36:01 +0200

    create_inception_pipeline.py

--- create_inception_pipeline.py
+++ create_inception_pipeline.py
@@ -1,11 +1,11 @@
 from gomatic import GoCdConfigurator, HostRestClient, ExecTask, GitMaterial
 
-configurator = GoCdConfigurator(HostRestClient("localhost:8153"))
+configurator = GoCdConfigurator(HostRestClient("docker:8153"))
 
 pipeline = configurator\
     .ensure_pipeline_group("inception")\
     .ensure_replacement_of_pipeline("inception")\
-    .set_git_material(GitMaterial("https://github.com/ivanmoore/inception.git",
+    .set_git_material(GitMaterial("https://github.com/yauma21/inception.git",
                                   polling=False))\
     .set_timer("0 0 * * * ?")  # run on the hour, every hour
 inception_job = pipeline.ensure_stage("inception").ensure_job("inception")
